22408b8f11d100dbd2a0c3be0fef26f171c851fd
nbogie <neillbogie@googlemail.com>
2023-02-07 23:49:20 +0000

    carve to connection point after hunt

--- src/core/carver.ts
+++ src/core/carver.ts
@@ -9,28 +9,23 @@ export function carveMazeMutates(maze: Maze): void {
     //   start location = hunt for new start location matching criteria (else null)
     let location: Cell | null = maze.randomCell();
     const visitedCells: Cell[] = [];
-    let counter = 1;
     while (location) {
         visitedCells.push(location);
         while (unvisitedNeighboursOf(location, maze, visitedCells).length > 0) {
             const unvis = unvisitedNeighboursOf(location, maze, visitedCells);
-            console.log("Neighbours of ", location, " are ", unvis.map(ns => ns.cell.id))
             const next = pick(unvis);
             carveFromToMutates(maze, location, next);
             location = next.cell;
-            counter++;
-            console.log("updating location to ", location)
             visitedCells.push(location);
         }
         location = huntForNewStart(maze, visitedCells);
-        //TODO: carve to connect new start location to an adjacent visited cell
 
         if (location) {
-            counter++;
-            console.log("hunted up new location: ", location)
+            //Carve to connect new start location to an adjacent *visited* cell
+            const possibleConnectionCells = visitedNeighboursOf(location, maze, visitedCells);
+            carveFromToMutates(maze, location, pick(possibleConnectionCells));
         }
     }
-    console.log({ counter, vcCount: visitedCells.length, maze })
 }
 function huntForNewStart(maze: Maze, visitedCells: Cell[]): Cell | null {
     return maze.cells.find(c =>
@@ -40,7 +35,6 @@ function huntForNewStart(maze: Maze, visitedCells: Cell[]): Cell | null {
 }
 
 export function carveFromToMutates(maze: Maze, from: Cell, to: DirectedCell) {
-    console.log("carving: ", from.id, " to ", to.cell.id, { from, to })
     from.wallDirs = from.wallDirs.filter(wd => wd !== to.dir);
     to.cell.wallDirs = to.cell.wallDirs.filter(wd => wd !== reciprocalDir(to.dir));
 }
@@ -49,24 +43,39 @@ export function getNeighbourInDirection(maze: Maze, fromCell: Cell, direction: D
     const offset: PosOffset = directionAsOffset(direction);
     return maze.get(fromCell.pos.x + offset.x, fromCell.pos.y + offset.y);
 }
-/** Get all neighbours including the direction the lie in */
-export function getAllNeighboursIncDirs(loc: Cell, maze: Maze) {
-    return allDirs.map((dir) => {
-        return { dir, cell: getNeighbourInDirection(maze, loc, dir) };
-    }).filter(n => n.cell) as DirectedCell[];
+
+/** Get all neighbours, including the direction the lie in */
+export function getAllNeighboursWithTheirDirs(loc: Cell, maze: Maze): DirectedCell[] {
+    return (
+        allDirs
+            .map((dir) => ({ dir, cell: getNeighbourInDirection(maze, loc, dir) }))
+            .filter(n => n.cell) as DirectedCell[]
+    );
 }
-export function unvisitedNeighboursOf(
-    loc: Cell,
-    maze: Maze,
-    visitedCells: Cell[]
-): DirectedCell[] {
-    const nsWithDirs = getAllNeighboursIncDirs(loc, maze);
-    return nsWithDirs.filter((nWithDir) => nWithDir.cell && isUnvisited(visitedCells, nWithDir.cell)) as DirectedCell[];
+
+export function unvisitedNeighboursOf(loc: Cell, maze: Maze, visitedCells: Cell[]): DirectedCell[] {
+    return getAllNeighboursWithTheirDirs(loc, maze)
+        .filter((nWithDir) => isUnvisited(visitedCells, nWithDir.cell));
+}
+
+export function visitedNeighboursOf(loc: Cell, maze: Maze, visitedCells: Cell[]): DirectedCell[] {
+    return getAllNeighboursWithTheirDirs(loc, maze)
+        .filter((nWithDir) => isVisited(visitedCells, nWithDir.cell));
+}
+
+function isVisited(visitedCells: Cell[], soughtCell: Cell): boolean {
+    return !!visitedCells.find(vc => vc.id === soughtCell.id)
 }
 
 function isUnvisited(visitedCells: Cell[], soughtCell: Cell): boolean {
-    return !visitedCells.find(vc => vc.id === soughtCell.id)
+    return !isVisited(visitedCells, soughtCell);
 }
+
+function isAdjactedToVisitedCell(c: Cell, visitedCells: Cell[], maze: Maze): boolean {
+    const neighbourCells = getAllNeighboursWithTheirDirs(c, maze).map(v => v.cell);
+    return neighbourCells.some(nc => visitedCells.find(vc => vc.id === nc.id));
+}
+
 
 export function pick<T>(arr: T[]): T {
     if (arr.length === 0) {
@@ -74,8 +83,3 @@ export function pick<T>(arr: T[]): T {
     }
     return arr[Math.floor(Math.random() * arr.length)];
 }
-function isAdjactedToVisitedCell(c: Cell, visitedCells: Cell[], maze: Maze): boolean {
-    const neighbourCells = getAllNeighboursIncDirs(c, maze).map(v => v.cell);
-    return neighbourCells.some(nc => visitedCells.find(vc => vc.id === nc.id));
-}
-
--- src/core/mazeGen.test.ts
+++ src/core/mazeGen.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { getAllNeighboursIncDirs } from "./carver";
+import { getAllNeighboursWithTheirDirs } from "./carver";
 import { createMaze } from "./mazeGen";
 test("", () => {
     const maze = createMaze(5, 5);
@@ -7,7 +7,7 @@ test("", () => {
     expect(mainCell).toBeTruthy();
     expect(mainCell?.id).toEqual(20)
     expect(mainCell?.pos).toEqual({ x: 4, y: 3 })
-    const neighbourCells = getAllNeighboursIncDirs(mainCell!, maze).map(ns => ns.cell)
+    const neighbourCells = getAllNeighboursWithTheirDirs(mainCell!, maze).map(ns => ns.cell)
     expect(
         new Set(neighbourCells.map(cell => cell.id)))
         .toEqual(new Set([15, 19, 25]));
