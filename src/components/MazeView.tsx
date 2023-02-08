96a5c8d46e30acc0ff5e5f929b70e8861b80f50b
nbogie <neillbogie@googlemail.com>
2023-02-08 00:20:31 +0000

    tidy maze view

--- src/components/MazeView.tsx
+++ src/components/MazeView.tsx
@@ -7,22 +7,15 @@ interface MazeViewProps {
 }
 export function MazeView({ maze }: MazeViewProps) {
 
-    const cellList = [];
-    for (let gY = 0; gY < maze.width; gY++) {
-        for (let gX = 0; gX < maze.width; gX++) {
-            const cell = maze.get(gX, gY);
-            if (!cell) {
-                throw new Error(`no cell at ${gX}, ${gY}`);
-            }
-            cellList.push(<CellView key={cell.id} cell={cell} />);
-        }
-    }
-
     //This, to tell css grid how many rows and cols.  Probably possible in pure css.
     const styleWithVar = { "--numColumnsInGrid": maze.width } as React.CSSProperties;
     return (
         <div className="mazeView" style={styleWithVar}>
-            {cellList}
+            {
+                maze.cells.map(
+                    cell => <CellView key={cell.id} cell={cell} />
+                )
+            }
         </div>
     );
 }
